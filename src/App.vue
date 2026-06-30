2ffbf72c2b88fb6787fb6d8e18947c8b721cdfa2
Jetross <jetrossneri07@gmail.com>
2026-06-30 20:49:17 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="flex min-h-screen items-center justify-center bg-gray-100">
+    <h1 class="text-3xl font-semibold">To-do List</h1>
+  </div>
+</template>
